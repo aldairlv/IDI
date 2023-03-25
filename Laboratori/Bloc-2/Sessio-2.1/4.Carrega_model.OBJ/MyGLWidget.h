4b9d288ed7992e5c1a574488fd742b50dad321e2
Aldair López Villalobos <aldairlv@protonmail.com>
2023-03-25 09:20:20 +0100

    -

---
+++ Laboratori/Bloc-2/Sessio-2.1/4.Carrega_model.OBJ/MyGLWidget.h
@@ -0,0 +1,33 @@
+// MyGLWidget.h
+#include "BL2GLWidget.h"
+#include "model.h"
+
+class MyGLWidget : public BL2GLWidget {
+  Q_OBJECT
+
+  public:
+    MyGLWidget(QWidget *parent=0) : BL2GLWidget(parent) {}
+    ~MyGLWidget();
+
+  protected:
+    virtual void paintGL();
+    virtual void initializeGL();
+    virtual void carregaShaders();
+    virtual void projectTransform();
+    virtual void viewTransform();
+    virtual void ini_camera();
+    virtual void creaBuffersModel();
+    GLuint projLoc;
+    GLuint viewLoc;
+    glm::vec3 obs;
+    glm::vec3 vrp;
+    glm::vec3 up;
+    float fov;
+    float ra;
+    float znear;
+    float zfar;
+    Model m;
+    GLuint VAO_model;
+  private:
+    int printOglError(const char file[], int line, const char func[]);
+};
